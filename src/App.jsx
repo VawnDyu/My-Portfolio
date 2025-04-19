65cbc18bb2989171567ef74dc8f480c58eb3b9fb
Vawndyu <vonnedewsalig@gmail.com>
2025-04-19 09:08:20 +0800

    Minimal updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Projects from './projects';
 import Home from './home';
 import Navbar from './navbar';
